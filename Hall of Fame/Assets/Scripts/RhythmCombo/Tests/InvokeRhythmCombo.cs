6db1f323ede0f0bea3e5908d5b5a2cdbc51e1542
Jason Lin <jlin116@ucsc.edu>
2018-01-21 19:00:08 -0800

    Created singleton RhythmCombo prefab
    Including helper classes and other prefabs for rhythm combo initialization.
    
    Currently can only properly be invoked for intitialization, it finds the RhythmComboCanvas in scene and initializes itself
    by adding more prefabs into the scene as the child object.
    
    Not ready for use yet

---
+++ Hall of Fame/Assets/Scripts/RhythmCombo/Tests/InvokeRhythmCombo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeRhythmCombo : MonoBehaviour
+{
+    private void Start()
+    {
+        RhythmCombo.instance.Test();
+        RhythmCombo.instance.Register(this.GetComponent<ComboPiece>());
+    }
+}
