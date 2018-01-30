768ad5c4272446a2ea06d2c977bab6e01d3287d9
Robin_BoyangZhou <z2367570158@gmail.com>
2018-01-29 17:10:39 -0800

    fixed animator

---
+++ Hall of Fame/Assets/Scripts/Fighting/HitContrller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitContrller : MonoBehaviour {
+
+    private int oldStateHash = 0;
+    private BoxState[] boxStates;
+
+
+	void Start () {
+        boxStates = GetComponentsInChildren<BoxState>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(gameObject.tag == "Player"&&other.tag == "Enemy")
+        {
+            AnimatorStateInfo stateInfo = gameObject.GetComponentInParent<Animator>().GetCurrentAnimatorStateInfo(0);
+
+            if (oldStateHash != stateInfo.fullPathHash)
+            {
+                other.gameObject.GetComponentInParent<Animator>().SetTrigger("Hit");
+            }
+
+
+            oldStateHash = stateInfo.fullPathHash;
+
+        }
+    }
+
+
+}
---
+++ Hall of Fame/Assets/Scripts/Fighting/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using Spine.Unity;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] private Vector2 moveSpeed = new Vector2(10,0);
+    [SerializeField] private int[] test;
+
+    private BoxState[] boxStates;
+    private Animator animator;
+    private Rigidbody2D rigidbody2d;
+
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        boxStates = GetComponentsInChildren<BoxState>();
+    }
+	
+	void Update () 
+    {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        float horizontal = Input.GetAxis("360Controller_LS_XAxis");
+        horizontal = ( horizontal == 0)? Input.GetAxis("Keyboard_Horizontal") : horizontal;
+        animator.SetFloat("WalkSpeed", horizontal);
+
+        if(stateInfo.IsName("Walk")||stateInfo.IsName("Back Walk"))
+        {
+            transform.position = rigidbody2d.position+horizontal*moveSpeed*Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("360Controller_Button_X") || Input.GetButtonDown("Keyboard_J"))
+        {
+            animator.SetTrigger("Punch");
+        }
+
+        if(Input.GetButtonDown("360Controller_Button_Y")||Input.GetButtonDown("Keyboard_K"))
+        {
+            animator.SetTrigger("Kick");
+        }
+
+
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+}
